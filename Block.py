e4509963cb42593b24c6634a28a605cb60e0a2df
Masayuki Osuka <maskore8@gmail.com>
2021-07-10 08:08:32 +0800

    add the latest files

--- Block.py
+++ Block.py
@@ -1,4 +1,4 @@
-import hashing
+import json, hashing
 
 class Block:
     def __init__(self, hash_previous_block, transactions, nonce):
@@ -7,13 +7,16 @@ class Block:
         self.nonce = nonce
 
     def get_hash(self):
+        data = self.get_dict()
+        json_data = json.dumps(data)
+        return hashing.hash(json_data)
+
+    def get_dict(self):
         transaction_hash_list = []
         for i in self.transactions:
             transaction_hash_list.append(i.get_hash())
-        data = {
+        return {
             "transaction_hashes": transaction_hash_list,
             "hash_previous_block": self.hash_previous_block,
             "nonce": self.nonce
         }
-        json_data = json.dumps(data)
-        return hashing.hash(json_data)
--- Main.py
+++ Main.py
@@ -1,3 +1,10 @@
-import Hashing
+from Transaction import Transaction, Coinbase
+from Blockchain import get_blockchain
+from Wallet import Wallet
+from Miner import Miner
 
-print(Hashing.hash("hello world"))
+wallet = Wallet()
+print(wallet.send_money([wallet.public_key], [50]))
+print(get_blockchain().blocks[0].get_hash())
+miner = Miner(own_public_key=wallet.public_key)
+
